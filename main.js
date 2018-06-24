99f283caf8c2e2e78d2c72f54d6aaf884cddbe7b
younusmahmood <younus23@gmail.com>
2018-06-23 23:14:40 -0500

    cleaned up images, added js

---
+++ main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  "use strict";
+
+  $(window).scroll(function() {
+    var nav = $(".navbar");
+    if ($(window).scrollTop() > 1000) {
+     $(".navbar").css("background-color", "#2B3137 !important");
+    } else {
+      $(".navbar").css("background-color", "#transparent");
+    }
+  });
+});
+
+$(".links a[href^='#']").on("click", function(e) {
+  e.preventDefault();
+
+  var hash = this.hash;
+
+  $("html, body").animate(
+    {
+      scrollTop: $(hash).offset().top
+    },
+    400,
+    function() {
+      window.location.hash = hash;
+    }
+  );
+});
+
+$(".btns a[href^='#']").on("click", function(e) {
+  e.preventDefault();
+
+  var hash = this.hash;
+
+  $("html, body").animate(
+    {
+      scrollTop: $(hash).offset().top
+    },
+    400,
+    function() {
+      window.location.hash = hash;
+    }
+  );
+});
